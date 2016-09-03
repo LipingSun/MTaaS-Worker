bc537b8446431737e659619f7f7d99b2af8f468c
LipingSun <liping.sun@sjsu.edu>
2016-09-03 10:57:18 -0700

    first version model

--- models/Device.js
+++ models/Device.js
@@ -2,7 +2,14 @@ var mongoose = require('mongoose');
 
 var Device = mongoose.model('Device', {
     imei: String,
-    cpu: String
+    brand: String,
+    model: String,
+    os_release: String,
+    os_version: String,
+    cpu: String,
+    memory: String,
+    status: String,
+    owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
 module.exports = Device;
