c682ce36275edb14c8721e585ba32f77b80868df
LipingSun <liping.sun@sjsu.edu>
2016-09-03 15:40:03 -0700

    Move device specs to spec attribute

--- models/Device.js
+++ models/Device.js
@@ -1,16 +1,10 @@
 var mongoose = require('mongoose');
 
 var Device = mongoose.model('Device', {
-    imei: String,
-    brand: String,
-    model: String,
-    os_release: String,
-    os_version: String,
-    cpu: String,
-    memory: String,
-    status: String,
     owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    adb_uri: String
+    status: String,
+    adb_uri: String,
+    spec: mongoose.Schema.Types.Mixed
 });
 
 module.exports = Device;
