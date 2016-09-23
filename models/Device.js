5f9add417bebece211de3b66e622d33c5492a927
LipingSun <liping.sun@sjsu.edu>
2016-09-22 23:08:44 -0700

    add occupant for device model

--- models/Device.js
+++ models/Device.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var Device = mongoose.model('Device', {
     owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    occupant: String,
     status: String,
     adb_uri: String,
     spec: mongoose.Schema.Types.Mixed
